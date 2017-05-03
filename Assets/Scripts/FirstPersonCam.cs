f3960bc794471de8d1a00fa3fcd2b9766591c0e4
Electron <devversitymail@gmail.com>
2017-05-03 19:42:00 +0530

    first commit

---
+++ Assets/Scripts/FirstPersonCam.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonCam : MonoBehaviour {
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update () {
+         if (Input.GetMouseButton(0)){
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+         } else {
+            transform.Rotate(new Vector3(0, 1, 0));
+         }
+    }
+}
---
+++ Assets/Scripts/LoadMainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMainMenu : MonoBehaviour {
+
+    public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+}
