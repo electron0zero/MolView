3ce7a255bd78ae84864d30cdd74c774a5ffdd480
Electron <devversitymail@gmail.com>
2017-05-10 01:08:30 +0530

    Multiple fixes - lighting fixes - zoom on touch devices - few small changes

--- Assets/Scripts/FollowTrackingCamera.cs
+++ Assets/Scripts/FollowTrackingCamera.cs
@@ -14,14 +14,14 @@ public class FollowTrackingCamera : MonoBehaviour
     public float max = 60f;
 
     // camera FOV for pinch to zoom (Zoom on Touch devices)
-    public float cameraFOVMin = 10f;
-    public float cameraFOVMax = 100f;
+    // private float cameraFOVMin = 10f;
+    // private float cameraFOVMax = 100f;
     // Options
-    public bool smoothZoom = true;
+    // public bool smoothZoom = true;
 
     // The movement amount when zooming.
-    public float zoomStep = 30f;
-    public float zoomSpeed = 5f;
+    private float zoomStep = 30f;
+    // private float zoomSpeed = 5f;
     private float heightWanted;
     private float distanceWanted;
 
@@ -45,7 +45,7 @@ public class FollowTrackingCamera : MonoBehaviour
             Debug.LogError("This camera has no target, you need to assign a target in the inspector.");
             return;
         }
-        // Debug.Log(zoomResult);
+
         zoomer();
     }
 
@@ -56,6 +56,7 @@ public class FollowTrackingCamera : MonoBehaviour
             // Store both touches.
             Touch touchZero = Input.GetTouch(0);
             Touch touchOne = Input.GetTouch(1);
+            
 
             // Find the position in the previous frame of each touch.
             Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
@@ -67,64 +68,52 @@ public class FollowTrackingCamera : MonoBehaviour
 
             // Find the difference in the distances between each frame.
             float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+            zoomToDistance(-(deltaMagnitudeDiff/100f));
 
-            // If the camera is orthographic...
-            Camera cam = GetComponent<Camera>();
-            if (cam.orthographic)
-            {
-                // ... change the orthographic size based on the change in distance between the touches.
-                cam.orthographicSize += deltaMagnitudeDiff * (zoomSpeed/10);
-
-                // Make sure the orthographic size never drops below zero.
-                cam.orthographicSize = Mathf.Max(cam.orthographicSize, 0.1f);
-            }
-            else
-            {
-                // Otherwise change the field of view based on the change in distance between the touches.
-                cam.fieldOfView += deltaMagnitudeDiff * (zoomSpeed/10);
-
-                // Clamp the field of view to make sure it's between 0 and 180.
-                cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, cameraFOVMin, cameraFOVMax);
-            }
         }
     }
 
     void zoomScroll(){
         // Record our mouse input.  If we zoom add this to our height and distance.
         float mouseInput = Input.GetAxis("Mouse ScrollWheel");
-        heightWanted -= zoomStep * mouseInput;
-        distanceWanted -= zoomStep * mouseInput;
+        zoomToDistance(mouseInput);
+    }
 
-        // Make sure they meet our min/max values.
-        heightWanted = Mathf.Clamp(heightWanted, min, max);
-        distanceWanted = Mathf.Clamp(distanceWanted, min, max);
+    void zoomToDistance(float distance){
+        // Debug.Log("distance to zoom:" + distance);
+            heightWanted -= zoomStep * distance;
+            distanceWanted -= zoomStep * distance;
 
-        if (smoothZoom){
-            // Linearly interpolate to achieve smooth camera effect
-            height = Mathf.Lerp(height, heightWanted, Time.deltaTime * zoomSpeed);
-            distance = Mathf.Lerp(distance, distanceWanted, Time.deltaTime * zoomSpeed);
-        } else {
-            // Rough Zoom effect
-            height = heightWanted;
-            distance = distanceWanted;
-        }
+            // Make sure they meet our min/max values.
+            heightWanted = Mathf.Clamp(heightWanted, min, max);
+            distanceWanted = Mathf.Clamp(distanceWanted, min, max);
 
-        // Post our result.
-        zoomResult = new Vector3(0f, height, -distance);
-        
-        // Set the camera position reference.
-        transform.position = target.position + zoomResult;
-        // Face the desired position.
-        transform.LookAt(target);
+            // if (smoothZoom){
+            //     // Linearly interpolate to achieve smooth camera effect
+            //     height = Mathf.Lerp(height, heightWanted, Time.deltaTime * zoomSpeed);
+            //     distance = Mathf.Lerp(distance, distanceWanted, Time.deltaTime * zoomSpeed);
+            // } else {
+                // Rough Zoom effect
+                height = heightWanted;
+                distance = distanceWanted;
+            // }
+
+            // Post our result.
+            zoomResult = new Vector3(0f, height, -distance);
+            
+            // Set the camera position reference.
+            transform.position = target.position + zoomResult;
+            // Face the desired position.
+            transform.LookAt(target);
     }
 
     void zoomer(){
-        //Check if we are running either in the Unity editor or in a standalone build.
-        #if UNITY_STANDALONE || UNITY_WEBPLAYER
-            zoomScroll();
-        //Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
-        #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+        //Check if we are running on iOS, Android, Windows Phone 8 or iPhone
+        #if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
             zoomTouch();
+        //anywhere else use mouse controls
+        #else
+            zoomScroll();
         #endif
         // End of mobile platform dependent compilation section started above with #if
     }
